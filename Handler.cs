b80fe243a666a29a92544908843fb59226e4a46c
Bimal Sharma <bimalsharma1@outlook.com>
2020-05-22 05:35:11 +1000

    handler change

--- Handler.cs
+++ Handler.cs
@@ -6,26 +6,38 @@ using System.Net;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using Amazon.Lambda.APIGatewayEvents;
+using GraphQLDynamo.GraphQL;
+using System;
+using GraphQL;
+using Microsoft.Extensions.DependencyInjection;
+using GraphQL.Types;
+using GraphQLDynamo.Interfaces;
 
-// [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 namespace GraphQLDynamo
 {
-    public class Handler
+    public class Handler: Schema
     {
+        private readonly IObjectGraphType _data = new ObjectGraphType();
+        public Handler()
+        {
+  
+        }
+
+        public Handler(IDependencyResolver resolver) : base(resolver)
+        {
+            Query = resolver.Resolve<LPQuery>();
+            _data = Query;
+        }
         public async Task<APIGatewayProxyResponse> GetData(APIGatewayProxyRequest request, ILambdaContext context)
         {
      
             context.Logger.LogLine($"Found data");
 
-            Message page = new Message
-            {
-                Data = "heya"
-            };
-
             var response = new APIGatewayProxyResponse
             {
                 StatusCode = (int)HttpStatusCode.OK,
-                Body = JsonConvert.SerializeObject(page)
+                Body = JsonConvert.SerializeObject(_data)
  
             };
 
---
+++ ServerlessDeploy.ps1
@@ -0,0 +1,17 @@
+﻿$Env:SLS_DEBUG = "*"
+
+#dotnet tool update -g Amazon.Lambda.Tools
+# dotnet lambda package --configuration release --framework netcoreapp2.1 --output-package ./../package/Alveo.Service.Venue.0.0.0.zip
+#dotnet lambda package --configuration release --framework netcoreapp3.1 --output-package ./bin/release/GraphQL.zip -h
+
+npm install serverless-domain-manager
+npm install serverless-pseudo-parameters
+#npm install serverless-plugin-existing-s3 --no-save --loglevel=error
+npm install  api-gateway-stage-tag-plugin
+npm install  serverless-tag-cloud-watch-logs
+npm install  serverless-dynamodb-pitr
+#serverless create_domain
+#serverless deploy --verbose
+serverless deploy --verbose
+
+#serverless remove
